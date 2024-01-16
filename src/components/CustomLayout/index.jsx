ec0f2f4b50ca0ebfc808b0eb32bfd92c8b9dd244
gabrielNZT <gabrielnunescesarino@gmail.com>
2024-01-16 20:04:12 -0300

    chg: layout

--- src/components/CustomLayout/index.jsx
+++ src/components/CustomLayout/index.jsx
@@ -1,10 +1,11 @@
 import React, { useLayoutEffect, useState } from 'react';
-import { LogoutOutlined, UserOutlined } from '@ant-design/icons';
+import { LogoutOutlined, UserOutlined, WhatsAppOutlined } from '@ant-design/icons';
 import { Avatar, Breadcrumb, Dropdown, Layout, Menu, Modal, theme } from 'antd';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { ROLE_KEY, TOKEN_KEY, USER_ID_KEY } from '../../constants';
 import './style.css';
 import { isAdmin } from '../Private';
+import patrocination from 'assets/ponto_zero.png';
 
 const { Header, Content, Footer } = Layout;
 
@@ -133,10 +134,27 @@ const CustomLayout = ({ children }) => {
                 style={{
                     textAlign: 'center',
                     position: 'sticky',
-                    bottom: 0
+                    bottom: 0,
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between'
                 }}
             >
+                <span style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+                    <p> Nossos canais: </p>
+                    <span style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+                        <WhatsAppOutlined className='wpp-icon' style={{ fontSize: '32px' }} onClick={() => window.open('https://chat.whatsapp.com/Kcw5UjQQcitLERZz4IEmTp', '_blank')} />
+                    </span>
+                </span>
+
                 BOLAO 2024 ©2023 Created by NZT
+
+                <span style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+                    <p> Patrocinadores: </p>
+                    <span style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+                        <img className='patrocinator' style={{ width: '96px', objectFit: 'fill' }} src={patrocination} alt='patrocinations' onClick={() => window.open('https://www.instagram.com/pontozero.pe/', '_blank')} />
+                    </span>
+                </span>
             </Footer>
         </Layout>
     );
--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,8 @@ export default defineConfig(({ mode }) => {
         provider: path.resolve(__dirname, 'src/provider'),
         constants: path.resolve(__dirname, 'src/constants'),
         utils: path.resolve(__dirname, 'src/utils'),
-        hooks: path.resolve(__dirname, 'src/hooks')
+        hooks: path.resolve(__dirname, 'src/hooks'),
+        assets: path.resolve(__dirname, 'src/assets')
         // Adicione mais aliases conforme necessário
       },
     },
