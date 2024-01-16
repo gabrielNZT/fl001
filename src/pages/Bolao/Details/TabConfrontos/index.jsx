ff5e1da60f65fb570063e3f554e2351b6998cb5e
gabrielNZT <gabrielnunescesarino@gmail.com>
2024-01-16 20:20:47 -0300

    FIX

--- src/pages/Bolao/Details/TabConfrontos/index.jsx
+++ src/pages/Bolao/Details/TabConfrontos/index.jsx
@@ -46,7 +46,11 @@ const TabConfrontos = ({ loadingDetails, isParticipating }) => {
                 }
             });
 
-            data.sort((a, b) => a.date.localeCompare(b.date));
+            data.sort((a, b) => {
+                const dateA = new Date(a.date.split('/').reverse().join('-'));
+                const dateB = new Date(b.date.split('/').reverse().join('-'));
+                return dateA - dateB;
+            });
 
             const groupByDate = {};
             let roudNumber = 1;
